d6c744792a430d5189b82a7d139f731005bafd96
codongcodong <47500862+codongcodong@users.noreply.github.com>
2020-10-31 21:55:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 const string* target;
 
 void usage(void){
-    puts("syntax : netfilter-test <host>");
-    puts("sample : netfilter-test test.gilgil.net");
+	puts("syntax : netfilter-test <host>");
+	puts("sample : netfilter-test test.gilgil.net");
 }
 
 void sigintHandler(int sig){
@@ -21,20 +21,20 @@ void sigintHandler(int sig){
 }
 
 int main(int argc, char* argv[]){
-    if (argc != 2) {
-        usage();
-        return -1;
-    }
+	if (argc != 2) {
+		usage();
+		return -1;
+	}
 
-    target = new string(argv[1]);               //target host
-    struct sigaction sigintAction;		//signal handler for program termination
+	target = new string(argv[1]);               //target host
+	struct sigaction sigintAction;		//signal handler for program termination
     
-    sigemptyset(&sigintAction.sa_mask);
+	sigemptyset(&sigintAction.sa_mask);
 	sigintAction.sa_flags = 0;
 	sigintAction.sa_handler = sigintHandler;
 	sigaction(SIGINT,&sigintAction,0);
     
-    system("./setRules.sh");                     //setting up iptable rules to use NFQUEUE
+	system("./setRules.sh");                     //setting up iptable rules to use NFQUEUE
 
-    filterPackets();
+	filterPackets();
 }
